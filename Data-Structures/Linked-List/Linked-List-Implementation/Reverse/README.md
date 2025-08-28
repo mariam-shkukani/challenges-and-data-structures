aebc501d7d0b1b893ae93a0adbdc891fc3eb1c75
mariam-shkukani <meroosh700@gmail.com>
2025-08-28 20:17:27 +0300

    add linked list and reverse function

---
+++ Data-Structures/Linked-List/Linked-List-Implementation/Reverse/README.md
@@ -0,0 +1,9 @@
+# Linked List Reverse Challenge 5-B  
+
+## Whiteboard
+
+![Whiteboard](./docs/reverse-whiteboarde.PNG)
+
+## Console Output
+
+![Console Output](./docs/console-output.PNG)
---
+++ Data-Structures/Linked-List/Linked-List-Implementation/Reverse/reverse.js
@@ -0,0 +1,24 @@
+
+const { LinkedList } = require('../linkedlist');
+
+function demo() {
+  const list = new LinkedList();
+  [1,2,3,4].forEach(n => list.append(n));
+  console.log('Original List:', 'Head ->', list.toArray().join(' -> '), '-> Null');
+
+  list.reverse();
+  console.log('Reversed List:', 'Head ->', list.toArray().join(' -> '), '-> Null');
+
+  // single node
+  const single = new LinkedList();
+  single.append(10);
+  console.log('Single original:', single.toArray());
+  single.reverse();
+  console.log('Single reversed:', single.toArray());
+
+  // empty
+  const empty = new LinkedList();
+  console.log('Empty reverse result:', empty.reverse()); // should be null
+}
+
+demo();
